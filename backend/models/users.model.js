84f17e699ffe542afa8a548492ffc1d21e1e9e9c
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-14 23:35:13 +0300

    Backend: Create DB connection + Users Model

---
+++ backend/models/users.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "Name is required",
+  },
+  email: {
+    type: String,
+    required: "Email is required",
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: "Password is required",
+  },
+  userType: {
+    type: Number,
+    required: "User Type is required",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
