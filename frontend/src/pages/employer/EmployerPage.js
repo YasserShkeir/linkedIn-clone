3f9c8b24bbc9e951f08437841c32694cf51bd180
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-17 03:33:44 +0300

    Frontend: Employer Jobs List Display

--- frontend/src/pages/employer/EmployerPage.js
+++ frontend/src/pages/employer/EmployerPage.js
@@ -1,6 +1,15 @@
+import GetJobs from "./GetJobs";
 import axios from "axios";
+import { useState, useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import Button from "../../components/button/Button";
+import classes from "./EmployerPage.module.css";
 
 const EmployerPage = () => {
+  const [selectState, setSelectState] = useState(true);
+  const [selectData, setSelectData] = useState("");
+  let navigate = useNavigate();
+
   const getJobs = async () => {
     const get = await axios.get("http://127.0.0.1:3000/users/getJobs", {
       headers: {
@@ -9,13 +18,30 @@ const EmployerPage = () => {
       },
     });
 
-    console.log(get);
+    const res = get;
+    setSelectData(res);
+    setSelectState(false);
+  };
+
+  useEffect(() => {
+    getJobs();
+  }, []);
+
+  const toCreateJob = () => {
+    navigate("/employer/createJob");
+    <Navigate replace to="/employer/createJob" />;
   };
 
   return (
     <>
-      <h1>Hello</h1>
-      <button onClick={getJobs}>Test</button>
+      <div>
+        <div className={classes.flex}>
+          <h1>Jobs List:</h1>
+          <Button text="Create Job" onClick={toCreateJob} />
+        </div>
+
+        <GetJobs option={selectState} data={selectData} />
+      </div>
     </>
   );
 };
